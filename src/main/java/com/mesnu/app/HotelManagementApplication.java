7302c5b87e8f20b792cd622317991fc713a070bd
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2022-11-10 13:19:42 +0500

    init

---
+++ src/main/java/com/mesnu/app/HotelManagementApplication.java
@@ -0,0 +1,14 @@
+package com.mesnu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HotelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementApplication.class, args);
+    }
+
+}
